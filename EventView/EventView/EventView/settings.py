c729a543403c30d2bae810041e2b140ae0244069
Mauricio Vergara <mauricio301051020@gmail.com>
2019-09-10 23:04:10 -0500

    second

--- EventView/EventView/EventView/settings.py
+++ EventView/EventView/EventView/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'EventView.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql'    ,
         'NAME': 'EventView',
         'USER': 'root',
-        'PASSWORD': 'cbn2016',
+        'PASSWORD': '',
         'HOST': 'localhost',
     }
 }
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
